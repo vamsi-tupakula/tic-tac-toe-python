c6ac92036c9421cfb8882d5ea468d7e765f5b248
Vamsi <vamsitupakula474@gmail.com>
2021-11-12 21:00:27 +0530

    next turn func added

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from tkinter import *;
 import random
 
+def next_turn(row,col):
+    global player;
+
+    if buttons[row][col]['text'] == "" and check_winner() is False:
+        if player == players[0]:
+            buttons[row][col]['text'] = player;
+            if check_winner() is False:
+                player = players[1];
+                label.config(text=(player + " turn"));
+            elif check_winner() is True:
+                label.config(text=(players[0] + " wins"));
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"));
+        else:
+            buttons[row][col]['text'] = player;
+            if check_winner() is False:
+                player = players[0];
+                label.config(text=(player + " turn"));
+            elif check_winner() is True:
+                label.config(text=(players[1] + " wins"));
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"));
+
+
+def check_winner():
+    pass;
+
 def empty_spaces():
     spaces = 9;
 
