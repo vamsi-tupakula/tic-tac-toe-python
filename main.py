f0203b3acce882c5fe9a3d70d89cb189e95f8fff
Vamsi <vamsitupakula474@gmail.com>
2021-11-13 18:10:35 +0530

    bg colors added

--- main.py
+++ main.py
@@ -28,16 +28,31 @@ def next_turn(row,col):
 def check_winner():
     for row in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
+            buttons[row][0].config(bg="green");
+            buttons[row][1].config(bg="green");
+            buttons[row][2].config(bg="green");
             return True;
     for col in range(3):
         if buttons[0][col]['text'] == buttons[1][col]['text'] == buttons[2][col]['text'] != "":
+            buttons[0][col].config(bg="green");
+            buttons[1][col].config(bg="green");
+            buttons[2][col].config(bg="green");
             return True;
     
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
+        buttons[0][0].config(bg="green");
+        buttons[1][1].config(bg="green");
+        buttons[2][2].config(bg="green");
         return True;
     elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
+        buttons[0][2].config(bg="green");
+        buttons[1][1].config(bg="green");
+        buttons[2][0].config(bg="green");
         return True;
     elif empty_spaces() is False:
+        for row in range(3):
+            for col in range(3):
+                buttons[row][col].config(bg="yellow");
         return "Tie";
     else:
         return False;
@@ -65,6 +80,7 @@ def new_game():
     for row in range(3):
         for col in range(3):
             buttons[row][col]['text'] = "";
+            buttons[row][col].config(bg="white");
 
 mywin = Tk();
 mywin.title("Tic Tac Toe");
@@ -93,6 +109,9 @@ for row in range(3):
                                    font=('consolas',40),
                                    width=4,
                                    height=1,
+                                   bg="white",
+                                   highlightbackground="black",
+                                   highlightthickness=2,
                                    command=lambda row=row,col=col:next_turn(row,col));
         buttons[row][col].grid(row=row,column=col);
 
